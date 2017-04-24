4073498a573fbbd253bddf700b1be4e83f6587fe
Eduard Kunce <eduard.kunce@gmail.com>
2017-04-24 13:22:00 +0200

    initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,12 @@
+var React = require('react')
+var Nav = require('Nav')
+
+var Main = (props) => {
+    return (
+        <div>          
+            <Nav/>  
+        </div>
+    )    
+}
+
+module.exports = Main
